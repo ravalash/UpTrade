193e7f4f18bbe9f01b37867f500360f7fbf15b4e
Ravalash <13k0873juhky@opayq.net>
2020-07-21 23:50:33 -0400

    Updating sequelize functions

--- server.js
+++ server.js
@@ -1,51 +1,33 @@
 const express = require("express");
-const mysql = require('mysql')
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3306;
-const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+// Requiring our models for syncing
+const db = require("./models");
 
-let connection;
-//Defines connection environment based on server location
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-   connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-  });
-}
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the MySql
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
 });
-
 
 
 
