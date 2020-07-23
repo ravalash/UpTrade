e3fd5a522c7cf5f62dbf678baf49d6736da5867f
Dooski <dmckeon495@gmail.com>
2020-07-23 18:22:16 -0400

    built out dashboard more; user card made; components for listings made using example data file

---
+++ client/src/components/NewListingsBox/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ListingItem from "../ListingItem"
+const Data = require("../../exampleData")
+
+function NewListingsBox() {
+    return (
+        <div className="card" >
+            <div className="card-body">
+                <h5 class="card-title">New Listings</h5>
+                <hr />
+                <div className="container">
+                    <ListingItem name={Data[0].name} platform={Data[0].platform} genre={Data[0].genre} img={Data[0].img} lf1={Data[0].lf1} lf2={Data[0].lf2} lf3={Data[0].lf3} lf4={Data[0].lf4} />
+                    <ListingItem name={Data[1].name} platform={Data[1].platform} genre={Data[1].genre} img={Data[1].img} lf1={Data[1].lf1} lf2={Data[1].lf2} lf3={Data[1].lf3} lf4={Data[1].lf4} />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewListingsBox
