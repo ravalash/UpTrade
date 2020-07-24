a67e619467c4e4f8552d8787bde667c6dee71682
Ravalash <13k0873juhky@opayq.net>
2020-07-23 20:54:16 -0400

    fixing routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 // import Dashboard from './pages/Dashboard';
 import Dashboard from "./pages/Dashboard.js";
 import Inventory from "./pages/Inventory.js";
 import Listings from "./pages/Listings.js";
 import SignIn from "./pages/SignIn.js";
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
-         <Nav />
     <Router>
-   <div id="wrapper">  
-          <Switch>
-              <Route exact path="/" component={Login} />
+      <div id="wrapper">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Login} />
           <Route exact path="/register" component={Register} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/inventory" component={Inventory} />
-            <Route exact path="/listings" component={Listings} />
-            <Route exact path="/signin" component={SignIn} />
-          </Switch>
-        </div>
-    </Router >
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/inventory" component={Inventory} />
+          <Route exact path="/listings" component={Listings} />
+          <Route exact path="/signin" component={SignIn} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,14 @@
 const router = require("express").Router();
-<<<<<<< Updated upstream
 
-
-
-=======
 const userRoutes = require("./user");
 const testRoutes = require("./test");
+
 // User routes
 router.use("/user", userRoutes);
 
 //Test Routes
 router.use("/test", testRoutes)
 
->>>>>>> Stashed changes
+
+
 module.exports = router;
