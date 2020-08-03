b2101e2544922510860ba80c7f1f65664ec52993
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-08-02 22:04:06 -0400

    Integrated login page with sign in card

--- client/src/components/SignInCard/index.js
+++ client/src/components/SignInCard/index.js
@@ -1,78 +1,78 @@
-import React, { useState } from "react";
-import { Link, Redirect } from "react-router-dom";
-import Axios from 'axios';
+// import React, { useState } from "react";
+// import { Link, Redirect } from "react-router-dom";
+// import Axios from 'axios';
 
 
-function SignInCard() {
+// function SignInCard() {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [redirect, setRedirect] = useState(null);
-    const [user, setUser] = useState({})
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [redirect, setRedirect] = useState(null);
+//     const [user, setUser] = useState({})
 
-    const login = async (e) => {
-        console.log(`login`);
-        e.preventDefault();
-        try {
-            const res = await Axios.post('/api/user/login', {
+//     const login = async (e) => {
+//         console.log(`login`);
+//         e.preventDefault();
+//         try {
+//             const res = await Axios.post('/api/user/login', {
 
 
-                email: email,
-                password: password,
+//                 email: email,
+//                 password: password,
 
-                // withCredentials: true,
+//                 // withCredentials: true,
 
-            })
-            if (res.data) {
-                setUser(res.data);
-                setRedirect('/dashboard');
-            }
-        }
-        catch (error) {
-            console.log(error);
-        };
+//             })
+//             if (res.data) {
+//                 setUser(res.data);
+//                 setRedirect('/dashboard');
+//             }
+//         }
+//         catch (error) {
+//             console.log(error);
+//         };
 
-    };
+//     };
 
-    if (redirect) {
-        return <Redirect to={{
-            pathname: redirect,
-            state: { user: user }
-        }} />
-    }
+//     if (redirect) {
+//         return <Redirect to={{
+//             pathname: redirect,
+//             state: { user: user }
+//         }} />
+//     }
 
-    return (
-        <div className="card sign-in-card mx-auto">
-            <div className="card-body">
-                <h5 className="card-title">Log-In</h5>
-                <p className="card-subtitle"><Link to="/register">Not registered? Click here to set up an account</Link></p>
-                <hr />
-                <form className="sign-in-form">
-                    <div className="form-group">
-                        <label htmlFor="email">Email address</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Enter email"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-success" onClick={login}>Login</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className="card sign-in-card mx-auto">
+//             <div className="card-body">
+//                 <h5 className="card-title">Log-In</h5>
+//                 <p className="card-subtitle"><Link to="/register">Not registered? Click here to set up an account</Link></p>
+//                 <hr />
+//                 <form className="sign-in-form">
+//                     <div className="form-group">
+//                         <label htmlFor="email">Email address</label>
+//                         <input
+//                             type="email"
+//                             className="form-control"
+//                             placeholder="Enter email"
+//                             onChange={(e) => setEmail(e.target.value)}
+//                         />
+//                     </div>
+//                     <div className="form-group">
+//                         <label htmlFor="exampleInputPassword1">Password</label>
+//                         <input
+//                             type="password"
+//                             className="form-control"
+//                             placeholder="Password"
+//                             onChange={(e) => setPassword(e.target.value)}
+//                         />
+//                     </div>
+//                     <button type="submit" className="btn btn-success" onClick={login}>Login</button>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default SignInCard
+// export default SignInCard
 
 
