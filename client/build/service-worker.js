02b5ee35283b4dd98979693e22d574318fecf280
Ravalash <13k0873juhky@opayq.net>
2020-08-06 16:27:43 -0400

    Small change to listing page

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df61111f9476bf01b0f4e93d4c18b1c6.js"
+  "/precache-manifest.8c76894f8ad421b45edd3a33f9ea211b.js"
 );
 
 workbox.clientsClaim();
